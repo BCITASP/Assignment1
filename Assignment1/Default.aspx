bd470bbf7a8769b4dc602a1ece334285d6b7858f
alan_lai <alan@ergostratus.com>
2015-01-24 00:08:15 -0800

    Added .NET jquery libraries from NuGet Search functionality properly returns correct filesnames.

--- Assignment1/Default.aspx
+++ Assignment1/Default.aspx
@@ -9,6 +9,27 @@
 <body>
     <form id="form1" runat="server">
     <div>
+        <asp:Label ID="lblSearchTerm" runat="server" Text="Search Term:"></asp:Label>
+        <asp:TextBox ID="txtSearchTerm" runat="server" Width="268px"></asp:TextBox>
+        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        <asp:RequiredFieldValidator ID="vldSearchTermRequired" runat="server" Display="Dynamic" ErrorMessage="A search term is required!" ControlToValidate="txtSearchTerm" ForeColor="Red"></asp:RequiredFieldValidator>
+        <asp:Panel ID="pnlResult" runat="server" Height="520px" Width="1017px">
+                    <asp:Label ID="lblFileName" runat="server" Text="File:"></asp:Label>
+        <asp:TextBox ID="txtFileName" runat="server" ReadOnly="True"></asp:TextBox>
+                    <asp:Label ID="lblResultCount" runat="server" Text="ResultCount"></asp:Label>
+                    <asp:Button ID="btnFirst" runat="server" Text="First" />
+                    <asp:Button ID="btnPrevious" runat="server" Text="Previous" />
+                    <asp:Button ID="btnNext" runat="server" Text="Next" />
+                    <asp:Button ID="btnLast" runat="server" Text="Last" />
+                    <br />
+                    <asp:TextBox ID="txtFileContent" runat="server" Height="456px" TextMode="MultiLine" Width="750px"></asp:TextBox>
+                    <br />
+                    <asp:ImageButton ID="btnPrint" runat="server" Height="67px" ImageUrl="~/images/printericon.png" Width="67px" />
+                    <asp:ImageButton ID="btnSave" runat="server" Height="67px" ImageUrl="~/images/diskicon.jpg" Width="67px" />
+        </asp:Panel>
+        <br />
+        <br />
+
     </div>
     </form>
 </body>
--- Assignment1/Default.aspx.cs
+++ Assignment1/Default.aspx.cs
@@ -1,17 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Collections;
 
 namespace Assignment1
 {
     public partial class Default : System.Web.UI.Page
     {
+        string[] searchfiles;
+        int currentFileIndex;
+        int lastFileIndex;
+        int resultCount;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                currentFileIndex = 0;
+                lastFileIndex = 0;
+                resultCount = 0;
+                searchfiles = Directory.GetFiles(Page.MapPath("~/files"));
+                ViewState["searchfiles"] = searchfiles;
+            } 
+            else
+            {
 
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            PerformSearch(txtSearchTerm.Text);
+        }
+
+        private void PerformSearch(string searchTerms)
+        {
+            string cleanedSearchTerms = RemoveExcludedWords(searchTerms);
+            ArrayList filenamesList = Search(cleanedSearchTerms);
+        }
+
+        private ArrayList Search(string cleanedSearchTerms)
+        {
+            ArrayList filenames = new ArrayList();
+            string[] cleanedSearchTermsArray = cleanedSearchTerms.Split(' ');
+            foreach(string file in (string[])ViewState["searchfiles"])
+            {
+                string content = File.ReadAllText(file);
+                foreach(string word in cleanedSearchTermsArray)
+                {
+                    if (content.Contains(word))
+                    {
+                        filenames.Add(file);
+                        break;
+                    }
+                }
+            }
+            return filenames;
+        }
+
+        private string RemoveExcludedWords(string searchTerms)
+        {
+            string[] excludedWordsArray = File.ReadAllText(MapPath("~/exclusion/exclusion.txt")).Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in excludedWordsArray)
+            {
+                searchTerms.Replace(word, "");
+            }
+            return searchTerms;
         }
     }
 }
